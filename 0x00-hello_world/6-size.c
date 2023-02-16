40969501f6ccb956c84cc6d6a4b9a568bd2f44de
bukola <abdulsamadbukola2005@gmail.com>
2023-02-16 13:54:57 -0800

    Error Corrected at line 3

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,16 @@
 */
 int main(void)
 {
-	printf("Size of a char: %d byte(s)\n", sizeof(char));
-	printf("Size of a int: %d bytes(s)\n", sizeof(int));
-	printf("Size of a long int: %ld bytes(s)\n", sizeof(long int));
-	printf("Size of a long long int: %lld bytes(s)\n", sizeof(long long int);
-	printf("Size of a float: %d bytes(s)\n", sizeof(float));
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a int: %lu bytes(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a float: %lu bytes(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }

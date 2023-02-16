517ecd17d69f52794dab07a1bc5435cea146a7a2
bukola <abdulsamadbukola2005@gmail.com>
2023-02-16 15:44:02 -0800

    Added block comment at top of file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,9 @@
+/*
+ * File: 4-puts.c
+ * Auth: Abdulsamad
+ * Desc: A program that reads in a user's initials and age,
+ *	 and prints them out.
+ */
 #include <stdio.h>
 /**
   * main - print out a line using puts function

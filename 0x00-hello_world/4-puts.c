c683417986f4dac11ceae12b0de45855f7e9b356
bukola <abdulsamadbukola2005@gmail.com>
2023-02-16 13:06:57 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  * main - print out a line using puts function
+  *
+  * Return: 0
+*/
+int main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

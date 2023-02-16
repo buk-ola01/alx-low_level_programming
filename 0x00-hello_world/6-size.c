2c62cf7db25b699c94d965c122721f21a78f3102
bukola <abdulsamadbukola2005@gmail.com>
2023-02-16 13:39:10 -0800

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+  * main - Print the size of varoius types on the computer
+  *
+  * Return - 0
+*/
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of a int: %d bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %ld bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lld bytes(s)\n", sizeof(long long int);
+	printf("Size of a float: %d bytes(s)\n", sizeof(float));
+	return (0);
+}

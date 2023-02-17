790e261a5f1f553e2824848a49e356c9be5d167b
bukola <abdulsamadbukola2005@gmail.com>
2023-02-17 15:54:09 -0800

    Task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+/*
+ * File: 9-print_comb.c
+ * Auth: Abdulsamad.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of three different digits,
+ *        in ascending order, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = i + 1; j < 9; j++)
+		{
+			for (k = j + 1; k < 10; k++)
+			{
+				putchar((i % 10) + '0');
+				putchar((j % 10) + '0');
+				putchar((k % 10) + '0');
+
+				if (i == 7 && j == 8 && k == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}

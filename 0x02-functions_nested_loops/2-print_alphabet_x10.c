255b28110c799af2a4e641c2d4737c84b24ff1db
bukola <abdulsamadbukola2005@gmail.com>
2023-02-21 04:06:17 -0800

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	int j;
+
+	while (i < 10)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		i++;
+	}
+}

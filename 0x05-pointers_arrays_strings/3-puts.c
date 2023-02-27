4f7231377673581a982117c9b110fb253c5e76ee
bukola <abdulsamadbukola2005@gmail.com>
2023-02-27 12:37:07 -0800

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - function that prints a string, followed by a new line, to stdout.
+ * @str: - string to be printed
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}

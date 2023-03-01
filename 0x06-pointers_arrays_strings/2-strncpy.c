d2bd73c7d74d86ceba4fac3f49619785eca34474
bukola <abdulsamadbukola2005@gmail.com>
2023-03-01 14:47:05 -0800

    Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncat - function that copies a string.
+ *
+ * @dest: - destination to be copied to.
+ * @src: -  source to be copied from.
+ * @n: - no of byetes to copy from src.
+ *
+ * Return: dest value.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i]; i++)
+	{
+		dest[i] = src[i];
+	}
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}

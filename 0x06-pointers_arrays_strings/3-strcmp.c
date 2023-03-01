f160d55956809f934600ff06ca47917e6f95a6cd
bukola <abdulsamadbukola2005@gmail.com>
2023-03-01 15:39:28 -0800

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcmp - function that compares two strings.
+ * @s1: - first string
+ * @s2: - second string
+ * Return: value of string
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int diff;
+
+	diff = s1[i] - s2[i];
+	while (diff == 0 && s1[i])
+	{
+		i++;
+	}
+
+	return (diff);
+}

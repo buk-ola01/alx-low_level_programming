2b91adef5cc8123c3f74df9ecc02d99a41db39de
bukola <abdulsamadbukola2005@gmail.com>
2023-03-05 13:41:46 -0800

    Advanced Task 101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - function that prints an integer.
+ * @n: integer to printed
+ */
+
+void print_number(int n)
+{
+	unsigned int num;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+
+	num = n;
+
+	if (num / 10)
+	{
+		print_number(num / 10);
+	}
+	_putchar((num % 10) + '0');
+}

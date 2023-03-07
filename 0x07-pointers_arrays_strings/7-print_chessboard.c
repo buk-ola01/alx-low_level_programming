b8b59f9b8083add2d853b186b7a2be2bffd50b4e
bukola <abdulsamadbukola2005@gmail.com>
2023-03-06 16:35:41 -0800

    Task 7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+/*
+ * File: 7-print_chessboard.c
+ * Auth: Abdulsamad.
+ */
+
+#include "main.h"
+
+/**
+ * print_chessboard - function that prints the chessboard.
+ * @a: The chessboard to be printed
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}

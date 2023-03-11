55851464c03104abbdbcd44876c33a232ca1e5b6
bukola <abdulsamadbukola2005@gmail.com>
2023-03-11 13:23:56 -0800

    Task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,44 @@
+/*
+ * File: 4-add.c
+ * Auth: Abdulsamad.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - program that adds positive numbers.
+ * @argc: argument count.
+ * @argv: Arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum = 0, j;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[i][j]; j++)
+			{
+				if (argv[i][j] < '0' || argv[i][j] > '9')
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+	}
+
+	return (0);
+}

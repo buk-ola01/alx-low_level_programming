add6e7c81505b2e6f4028f51971d2964e09d25cf
bukola <abdulsamadbukola2005@gmail.com>
2023-03-11 11:36:17 -0800

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,36 @@
+/*
+ * File: 3-mul.c
+ * Auth: Abdulsamad.
+ */
+
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - a program that multiplies two numbers.
+ * @argc: No of arguements.
+ * @argv: Array of pointers
+ *
+ * Return: Always 0(Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, mul = 1;
+
+	if (argc == 3)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul = mul * atoi(argv[i]);
+		}
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+
+	return (0);
+}

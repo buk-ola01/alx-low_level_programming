25ba1ce7d7e2dbe954506c891d4b8f1488302384
bukola <abdulsamadbukola2005@gmail.com>
2023-03-16 07:35:31 -0700

    Trying to pass check 8

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= len2)
 		n = len2;
 
-	concat = malloc((len1 + n) + 1);
+	concat = malloc(sizeof(char) * (len1 + n + 1));
 
 	if (concat == NULL)
 		return (NULL);

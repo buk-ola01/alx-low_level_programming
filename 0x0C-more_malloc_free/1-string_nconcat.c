bb719cfd4bb787d6384214438a6139a3fd966999
bukola <abdulsamadbukola2005@gmail.com>
2023-03-16 05:33:17 -0700

    Change memory allocated

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -28,7 +28,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	concat = malloc((len1 + len2) + 1);
+	concat = malloc((len1 + n) + 1);
 
 	if (concat == NULL)
 		return (NULL);

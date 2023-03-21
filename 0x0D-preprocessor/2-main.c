3defea9dad60f4fdd77736be35c96f0faabe4bd9
bukola <abdulsamadbukola2005@gmail.com>
2023-03-20 17:38:30 -0700

    Task 2 Completed

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,19 @@
+/*
+ * File: 2-main.c
+ * Auth: Abdulsamad.
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file of the program was complied from,
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}

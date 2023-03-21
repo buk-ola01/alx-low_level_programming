779fee9a1e2486b92ca692ff04d8c71d23575cf9
bukola <abdulsamadbukola2005@gmail.com>
2023-03-20 17:45:12 -0700

    Task 3 Completed

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/*
+ * File: 3-function_like_macro.h
+ * Desc: Header file that defines a function-like macro ABS(X)
+ *	that computes the absolute value of a number X
+ */
+
+#define ABS(X) ((X) < (0) ? (-X) : (X))
+
+#endif

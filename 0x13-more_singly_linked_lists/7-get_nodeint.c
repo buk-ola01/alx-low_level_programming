a4d7b6768323baab994c0c3689141357be289c98
bukola <abdulsamadbukola2005@gmail.com>
2023-04-04 16:17:25 -0700

    Task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,10 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *temp1 = head;
-	unsigned int i;
+	size_t i;
+
+	if (head == NULL)
+		return (NULL);
 
 	for (i = 0; i < index; i++)
 	{

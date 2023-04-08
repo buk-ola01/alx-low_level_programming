1fd99df51dea8a63ec2ba86acc981755ffde92d6
bukola <abdulsamadbukola2005@gmail.com>
2023-04-08 08:01:04 -0700

    Used _putchar instead

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -13,10 +13,10 @@ void print_binary(unsigned long int n)
 	{
 		if (n >> 1)
 			print_binary(n >> 1);
-		printf("%c", (n & 1) ? '1' : '0');
+		_putchar((n & 1) + '0');
 	}
 	else
 	{
-		printf("%c", '0');
+		_putchar('0');
 	}
 }
